106c855dc8edaf50e12916a350b29783f7896e40
ilya_kashkarev <ilya.kashkarev@nuance.com>
2016-12-03 01:11:07 +0100

    implemented default meeting request

---
+++ src/main/java/org/ilya/scheduler/request/DefaultMeetingRequest.java
@@ -0,0 +1,44 @@
+package org.ilya.scheduler.request;
+
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+
+public class DefaultMeetingRequest implements MeetingRequest {
+
+    private final RequestType type;
+    private Meeting meeting;
+    private DateTime submissionTime;
+
+    public DefaultMeetingRequest(RequestType type, DateTime submissionTime) {
+        this.type = type;
+        this.submissionTime = submissionTime;
+    }
+
+    public void withData(MeetingDetails details,
+                         DateTime start, DateTime end) {
+        this.meeting = new Meeting(this, details, start, end);
+    }
+
+    public void withData(MeetingDetails details,
+                         DateTime start, Duration duration) {
+        this.meeting = new Meeting(this, details, start, duration);
+    }
+
+    @Override
+    public RequestType getType() {
+        return type;
+    }
+
+    @Override
+    public Meeting getData() {
+        if (meeting == null) {
+            throw new IllegalStateException("Cannot retrieve requested meeting, because it was not set");
+        }
+        return meeting;
+    }
+
+    @Override
+    public DateTime getSubmissionTime() {
+        return null;
+    }
+}
